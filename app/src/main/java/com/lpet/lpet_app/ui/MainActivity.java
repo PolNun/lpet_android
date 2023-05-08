e75c598ff781b685583c6dca438a2e442c421921
Pablo Nunez <polnunez@outlook.com>
2023-05-08 16:59:18 -0300

    Imagenes convertidas a drawables

--- app/src/main/java/com/lpet/lpet_app/ui/MainActivity.java
+++ app/src/main/java/com/lpet/lpet_app/ui/MainActivity.java
@@ -3,14 +3,21 @@ package com.lpet.lpet_app.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.lpet.lpet_app.R;
+import com.lpet.lpet_app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
     }
 }
