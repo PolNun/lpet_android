4258e0a51e56d3624abb72074602df185bc17349
godoy <godoyeli85@gmail.com>
2023-05-09 17:37:20 -0300

    onclick hecho en el boton aun no tienes cuenta

--- app/src/main/java/com/lpet/lpet_app/ui/login/LoginFragment.java
+++ app/src/main/java/com/lpet/lpet_app/ui/login/LoginFragment.java
@@ -1,61 +1,53 @@
 package com.lpet.lpet_app.ui.login;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.lpet.lpet_app.R;
+import com.lpet.lpet_app.databinding.FragmentLoginBinding;
+import com.lpet.lpet_app.ui.MainActivity;
 import com.lpet.lpet_app.ui.registro.RegistroFragment;
 
 public class LoginFragment extends Fragment {
+private FragmentLoginBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistroFragment newInstance(String param1, String param2) {
-        RegistroFragment fragment = new RegistroFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        binding = FragmentLoginBinding.inflate(getLayoutInflater());
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        TextView tvRegistrarte = view.findViewById(R.id.tvRegistrate);
+
+        tvRegistrarte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarFragmenteRegistro(v);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+
+    }
+
+    private void cargarFragmenteRegistro(View v) {
+        RegistroFragment registroFragment = new RegistroFragment();
+        FragmentTransaction transaccion = getFragmentManager().beginTransaction();
+        transaccion.replace(R.id.fragment_container,registroFragment);
+        transaccion.addToBackStack(null);
+        transaccion.commit();
     }
 }
