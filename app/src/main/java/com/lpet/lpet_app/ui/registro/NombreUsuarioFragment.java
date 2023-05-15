82838ae482f73e86dc8f7e671592fa94258f9b2c
Pablo Nunez <polnunez@outlook.com>
2023-05-15 12:28:02 -0300

    Navegacion entre fragments hecha

--- app/src/main/java/com/lpet/lpet_app/ui/registro/NombreUsuarioFragment.java
+++ app/src/main/java/com/lpet/lpet_app/ui/registro/NombreUsuarioFragment.java
@@ -7,29 +7,52 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.lpet.lpet_app.R;
+import com.lpet.lpet_app.databinding.FragmentNombreUsuarioBinding;
 
 public class NombreUsuarioFragment extends Fragment {
     private String correo_electronico;
     private String contrasena;
 
+    FragmentNombreUsuarioBinding binding;
+
     public NombreUsuarioFragment() {
     }
 
-    public NombreUsuarioFragment(String correo_electronico, String contrasena) {
-        this.correo_electronico = correo_electronico;
-        this.contrasena = contrasena;
+    public static NombreUsuarioFragment newInstance(String correo_electronico, String contrasena) {
+        NombreUsuarioFragment fragment = new NombreUsuarioFragment();
+        Bundle args = new Bundle();
+        args.putString("correo_electronico", correo_electronico);
+        args.putString("contrasena", contrasena);
+        fragment.setArguments(args);
+
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        correo_electronico = getArguments().getString("correo_electronico");
+        contrasena = getArguments().getString("contrasena");
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nombre_usuario, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentNombreUsuarioBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        inicializar();
+
+        return view;
+    }
+
+    private void inicializar() {
+        binding.btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), correo_electronico, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/lpet/lpet_app/ui/registro/RegistroFragment.java
+++ app/src/main/java/com/lpet/lpet_app/ui/registro/RegistroFragment.java
@@ -3,14 +3,20 @@ package com.lpet.lpet_app.ui.registro;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lpet.lpet_app.R;
 import com.lpet.lpet_app.databinding.FragmentRegistroBinding;
 
 public class RegistroFragment extends Fragment {
+    String correo_electronico;
+    String contrasena;
     private FragmentRegistroBinding binding;
 
     @Override
@@ -36,10 +42,22 @@ public class RegistroFragment extends Fragment {
             }
         });
 
-        binding.btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+        binding.btnContinuarRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                correo_electronico = binding.etCorreoElectronicoRegistro.getText().toString();
+                contrasena = binding.etContrasenaRegistro.getText().toString();
 
+                NombreUsuarioFragment nombreUsuarioFragment = new NombreUsuarioFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("correo_electronico", correo_electronico);
+                bundle.putString("contrasena", contrasena);
+                nombreUsuarioFragment.setArguments(bundle);
+
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, nombreUsuarioFragment)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
     }
