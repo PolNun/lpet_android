1efe10fc59bdf4d023d31b29f36240552cebb636
Pablo Nunez <polnunez@outlook.com>
2023-08-27 19:30:13 -0300

    Modelo de chat creado

---
+++ app/src/main/java/com/lpet/lpet_app/models/Message.java
@@ -0,0 +1,40 @@
+package com.lpet.lpet_app.models;
+
+public class Message {
+    private String sender;
+    private String content;
+    private String timestamp;
+
+    public Message() {
+    }
+
+    public Message(String sender, String content, String timestamp) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
